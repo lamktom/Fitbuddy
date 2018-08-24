052008341dd17b472f84f89778ea20a3f527580e
lamktom <lamktom@yahoo.com>
2018-08-23 21:52:38 -0700

    fixed models

--- models.js
+++ models.js
@@ -1,14 +1,15 @@
+//done
 "use strict"; 
 
 const mongoose = require("mongoose"); 
 
 const workoutSchema = mongoose.Schema({
-	workouts: [
+	workout: {
 		workoutName: { type: String, required: true }, 
 		musclesWorked: { type: String, required: true },
 		equipment: { type: String, required: true },
 		video: { type: String }
-	]	
+	}	
 }); // end workoutSchema  
 
 workoutSchema.methods.serialize = function() {
