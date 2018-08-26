859c410520e378040bed43aa83f66813359f12c5
lamktom <lamktom@yahoo.com>
2018-08-25 23:15:18 -0700

    Updated config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 exports.DATABASE_URL = 
 	process.env.DATABASE_URL || "mongodb://fitbuddy-user:fitbuddy-user1@ds223542.mlab.com:23542/fitbuddy-db"; 
 exports.TEST_DATABASE_URL = 
-	process.env.TEST_DATABASE_URL || "mongodb://localhost/test-fitbuddy-app"; //create database and change this 
+	process.env.TEST_DATABASE_URL || "mongodb://<dbuser>:<dbpassword>@ds133152.mlab.com:33152/test-fitbuddy-db"; //create database and change this 
 exports.PORT = process.env.PORT || 8080; 
 
