a87f4b8b1ef0cae1d565f6fe75180319b1b44f1f
lamktom <lamktom@yahoo.com>
2018-08-26 20:13:30 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ const { Workout } = require("./models");
 const app = express();
 app.use(express.json()); 
 
+app.use(express.static('public'));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
+
 // GET request to /workouts 
 app.get("/workouts", (req, res) => {
   Workout.find()
@@ -33,7 +39,31 @@ app.get("/workouts", (req, res) => {
 // GET request by ID 
 app.get("/workouts/:id", (req, res) => {
   Workout.findById(req.params.id)
-  
+  .then(restaurant => res.json(restaurant.serialize()))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({ message: "Whoops, looks like something went wrong" });
+  });
+});
+
+// POST request 
+app.post("/workouts", (req, res) => {
+
+}); 
+
+// PUT request
+app.put("/workouts/:id", (req, res) => {
+
+});
+
+// DELETE request 
+app.delete("/workouts/:id", (req, res) =. {
+
+});
+
+// catch all endpoint if client makes request to non-existent endpoint
+app.use("*", function(req, res) {
+  res.status(404).json({ message: "404 Not Found" });
 });
 
 
@@ -51,49 +81,41 @@ app.get("/workouts/:id", (req, res) => {
 
 
 
+// declare server 
+let server; 
 
+function runServer() {
+	const port = process.env.PORT || 8080; 
+	return new Promise((resolve, reject) => {
+		server = app
+		.listen(port, () => {
+			console.log(`Your app is listening on port ${port}`); 
+			resolve(server); 
+		})
+		.on('error', err => {
+			reject(err); 
+		});
+	});
+} // end runServer 
 
-// app.use(express.static('public'));
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log("Closing server");
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+} // end closeServer
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/public/index.html");
-// });
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
 
-// // declare server 
-// let server; 
-
-// function runServer() {
-// 	const port = process.env.PORT || 8080; 
-// 	return new Promise((resolve, reject) => {
-// 		server = app
-// 		.listen(port, () => {
-// 			console.log(`Your app is listening on port ${port}`); 
-// 			resolve(server); 
-// 		})
-// 		.on('error', err => {
-// 			reject(err); 
-// 		});
-// 	});
-// } // end runServer 
-
-// function closeServer() {
-//   return new Promise((resolve, reject) => {
-//     console.log("Closing server");
-//     server.close(err => {
-//       if (err) {
-//         reject(err);
-//         return;
-//       }
-//       resolve();
-//     });
-//   });
-// } // end closeServer
-
-// // if server.js is called directly (aka, with `node server.js`), this block
-// // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
-// if (require.main === module) {
-//   runServer().catch(err => console.error(err));
-// }
-
-// module.exports = { app, runServer, closeServer };
+module.exports = { app, runServer, closeServer };
 
