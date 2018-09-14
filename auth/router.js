3e520168b51c803bd0b00f3e67ad018eb4f76d67
lamktom <lamktom@yahoo.com>
2018-09-13 22:39:00 -0700

    Finished most of JWT and started login and signup html

---
+++ auth/router.js
@@ -0,0 +1,44 @@
+'use strict'; 
+const express = require('express');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken'); 
+
+const config = require('../config');
+const router = express.Router(); 
+
+const createAuthToken = function(user) {
+	return jwt.sign({user}, config.JWT_SECRET, {
+		subject: user.username,
+		expiresIn: config.JWT_EXPIRY,
+		algorithm: 'HS256'
+	});
+};
+
+const localAuth = passport.authenticate('local', {session: false}); 
+
+router.use(bodyParser.json()); 
+router.use(bodyParser.urlencoded({
+	extended: true 
+}));
+
+//user provides username and password to to login 
+//runs through localAuth middleware to handle auth
+//if auth successful, creates and responds with a JWT
+router.post('/login', localAuth, (req, res) => {
+	console.log('authenticated user');
+	const authToken = createAuthToken(req.user.serialize()); 
+
+	res.json({token: authToken});
+})
+
+const jwtAuth = passport.authenticate('jwt', {session: false}); 
+
+//user exchanges a valid JWT for a new one with a later expiration
+router.post('/refresh', jwtAuth, (req, res) => {
+	console.log('authenticated user');
+	const authToken = createAuthToken(req.user);
+	res.json({authToken}); 
+});
+
+module.exports = {router};
---
+++ auth/strategies.js
@@ -0,0 +1,56 @@
+'use strict'; 
+
+const { Strategy: LocalStrategy } = require('passport-local'); 
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+
+const { User } = require('../users/models'); 
+const { JWT_SECRET } = require('../config'); 
+
+const LocalStrategy = new LocalStrategy((username,password, callback) => {
+  
+  let user;
+  User.findOne({ username })
+    .then(_user => {
+      user = _user;
+      if (!user) { 
+        // Return a rejected promise so we break out of the chain of .thens.
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Could not find user'
+        }); 
+      }
+      return user.validatePassword(password);
+    })
+    // password invalid
+    .then(isValid => {
+      if (!isValid) {
+        return Promise.reject({
+          reason: 'LoginError',
+          message: 'Incorrect password'
+        });
+      }
+        // password is valid, return user
+      return callback(null, user);
+    })
+    .catch(err => {
+      if (err.reason === 'LoginError') {
+        return callback(null, false, err);
+      }
+      return callback(err, false);
+    });
+});
+
+const jwtStrategy = new JwtStrategy(
+  {
+    secretOrKey: JWT_SECRET,
+    // Look for the JWT as a Bearer auth header
+    jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
+    // Only allow HS256 tokens - the same as the ones we issue
+    algorithms: ['HS256']
+  },
+  (payload, done) => {
+    done(null, payload.user);
+  }
+);
+
+module.exports = { localStrategy, jwtStrategy };
