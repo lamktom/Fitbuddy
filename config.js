328d367726b8fd733c0292774c0ae206ac4411d6
Tom Lam <lamktom@yahoo.com>
2018-10-07 14:57:46 -0700

    fixed registering users issue

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 "use strict"; 
 
-// exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://fitbuddy-user:fitbuddy-user1@ds223542.mlab.com:23542/fitbuddy-db";
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017/fitbuddy"; 
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://fitbuddy-user:fitbuddy-user1@ds223542.mlab.com:23542/fitbuddy-db";
+// exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017/fitbuddy"; 
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "mongodb://test-fitbuddy-user:test-fitbuddy-user1@ds133152.mlab.com:33152/test-fitbuddy-db"; 
 exports.PORT = process.env.PORT || 8080; 
 
