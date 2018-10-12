c2f7acfd515e0fd3898ad8fe4bfec03eb8bbbac1
lamktom <lamktom@yahoo.com>
2018-10-11 22:43:07 -0700

    Updated home.html and app.js

--- public/app.js
+++ public/app.js
@@ -1,8 +1,21 @@
 // once page loads 
 $(function() {
+	getUserWorkouts(updateSessionStorageWithWorkouts); 
+	getWorkoutsFromSessionStorage(renderWorkouts); 
+
 	// event listeners
-	
 
+	// add workout 
 
+	// delete workout 
+
+	// update workout 
+
+	// search workout from database 
+
+	// Rendering functions
+	$('#refresh-token-btn').on('click', function() {    
+  	getAuthToken(addAuthToken);
+  })
 
 });
